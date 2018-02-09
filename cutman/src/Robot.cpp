7f20759e35f19df5626f2b8fc71f1f7f66d44adc
Jason Markham <jasonmarkham@gmail.com>
2018-02-09 11:23:22 -0800

    new project

---
+++ cutman/src/Robot.cpp
@@ -0,0 +1,14 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include <WPILib.h>
+
+struct Robot :  TimedRobot {
+
+};
+
+START_ROBOT_CLASS(Robot)
