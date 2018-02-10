64347f1a0a2fffdbf6cb43134c2e7dbb2f5f6462
jasonnn <jasonmarkham@gmail.com>
2018-02-09 18:25:51 -0800

    add subsystems to robot

--- cutman/src/Robot.cpp
+++ cutman/src/Robot.cpp
@@ -3,13 +3,22 @@
 #include <WPILib.h>
 #include "RobotMap.h"
 #include "Subsystems/Bucket.h"
+#include "Subsystems/Chassis.h"
 #include "Subsystems/Clamp.h"
+#include "Subsystems/ClampTilt.h"
+#include "Subsystems/UpperTilt.h"
+#include "Subsystems/Winch.h"
+
 struct Robot : TimedRobot {
 
 	RobotMap robotMap;
 
     Bucket bucket{robotMap.bucketMotor,robotMap.bucketEncoder};
+	Chassis chassis{robotMap.differentialDrive};
     Clamp clamp{robotMap.clampMotor,robotMap.clampEncoder};
+    ClampTilt clampTilt{robotMap.clampTiltMotor,robotMap.clampTiltEncoder};
+    UpperTilt upperTilt{robotMap.upperTiltMotor,robotMap.upperTiltEncoder};
+    Winch winch{robotMap.winch};
 
     Robot(){
     }
